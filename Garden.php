c779e3deb5d70fe43f0ff47b8afb69454c4d5763
kondratievden <76614479+kondratievden@users.noreply.github.com>
2024-12-03 08:55:25 +0300

    Create Garden.php

---
+++ Garden.php
@@ -0,0 +1,21 @@
+<?php
+
+class Garden {
+    private $treeIds = [];
+    private $typeIdent = ['apple'=> [0, 40, 50] , 'pear' => [1, 0, 20]];
+    private static $nextId = 1;
+
+    public function addTrees($type, $quantity) {
+        
+        for ($i = 0; $i < $quantity; $i++) {
+            // Создаем  id четный/нечетный в зависимости от типа дерева
+            $id = self::$nextId * 2 + $this->typeIdent[$type][0];
+            $this->treeIds[$id] = rand($this->typeIdent[$type][1], $this->typeIdent[$type][2]);
+            self::$nextId++;
+        }
+    }
+    //метожд возвращает массив деревьев с id и кол-вом фруктов для каждого дерева
+    public function getTreeIds() {
+        return $this->treeIds;
+    }
+}
